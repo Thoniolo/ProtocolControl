b39cb84b0f902528bf9caa290797940dbd845c23
thomttorres <thomttorres@hotmail.com>
2023-12-09 01:45:53 -0300

    Add project files.

---
+++ ProtocolControl/ProtocolForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ProtocolControl
+{
+    partial class ProtocolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.btnGenerateProtocol = new System.Windows.Forms.Button();
+            this.btnViewProtocols = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Location = new System.Drawing.Point(91, 82);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(54, 13);
+            this.lblCompany.TabIndex = 5;
+            this.lblCompany.Text = "Empresa :";
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(160, 79);
+            this.txtCompany.MaxLength = 6;
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(159, 20);
+            this.txtCompany.TabIndex = 4;
+            // 
+            // btnGenerateProtocol
+            // 
+            this.btnGenerateProtocol.Location = new System.Drawing.Point(210, 105);
+            this.btnGenerateProtocol.Name = "btnGenerateProtocol";
+            this.btnGenerateProtocol.Size = new System.Drawing.Size(109, 35);
+            this.btnGenerateProtocol.TabIndex = 3;
+            this.btnGenerateProtocol.Text = "Gerar Protocolo";
+            this.btnGenerateProtocol.UseVisualStyleBackColor = true;
+            this.btnGenerateProtocol.Click += new System.EventHandler(this.btnGenerateProtocol_Click);
+            // 
+            // btnViewProtocols
+            // 
+            this.btnViewProtocols.Location = new System.Drawing.Point(94, 105);
+            this.btnViewProtocols.Name = "btnViewProtocols";
+            this.btnViewProtocols.Size = new System.Drawing.Size(110, 35);
+            this.btnViewProtocols.TabIndex = 6;
+            this.btnViewProtocols.Text = "Exibir Protocolos";
+            this.btnViewProtocols.UseVisualStyleBackColor = true;
+            this.btnViewProtocols.Click += new System.EventHandler(this.btnViewProtocols_Click);
+            // 
+            // ProtocolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 225);
+            this.Controls.Add(this.btnViewProtocols);
+            this.Controls.Add(this.lblCompany);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.btnGenerateProtocol);
+            this.Name = "ProtocolForm";
+            this.Text = ".:: Gerar Protocolo ::.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Button btnGenerateProtocol;
+        private System.Windows.Forms.Button btnViewProtocols;
+    }
+}
---
+++ ProtocolControl/ProtocolForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtocolControl
+{
+    public partial class ProtocolForm : Form
+    {
+        private static readonly object token = new object();
+        private static List<Protocol> protocols;
+
+        public ProtocolForm()
+        {
+            protocols = LoadFile();
+            InitializeComponent();
+        }
+
+        /* Ao receber id da empresa se gera o protocolo sequencial conforme regra 
+        XXXXXX = Número identificador da empresa: 6 dígitos numéricos. Ex: Empresa A = 319708
+		AAAAMMDD = Ano, Mês e Dia. Ex: 20230921
+		NNNNNN = Número sequencial do protocolo: 6 dígitos numéricos 
+        Depois é salvo os protocolos em um arquivo txt para se utilizado os registros nas próximas gerações */
+        private void btnGenerateProtocol_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtCompany.Text))
+            {
+                MessageBox.Show("Deve ser informado código identificador da empresa!");
+                txtCompany.Focus();
+                return;
+            }
+
+            #region Teste multi processos
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    ThreadStart start = () => ProcessProtocol(txtCompany.Text);
+            //    new Thread(start).Start();
+            //}
+            #endregion
+
+            ProcessProtocol(txtCompany.Text);
+        }
+
+        private static void ProcessProtocol(string company)
+        {
+            lock (token)
+            {
+                Protocol protocol = protocols.Find(x => x.IdComapany == company) ?? null;
+
+                protocols.Remove(protocol);
+                var protocolNew = GenerateProtocol(protocol, company);
+                protocols.Add(protocolNew);
+                SaveFile(protocols);
+            }
+        }
+
+        private static Protocol GenerateProtocol(Protocol protocol, string company)
+        {
+            var dateCurrent = DateTime.Now.ToString("yyyyMMdd");
+            var sequenceCurrent = protocol == null || protocol.Date != dateCurrent ? 1 : Convert.ToInt32(protocol.Sequence) + 1;
+
+            return new Protocol { IdComapany = company, Date = dateCurrent, Sequence = sequenceCurrent.ToString("D" + 6) };
+        }
+
+
+        private List<Protocol> LoadFile()
+        {
+            var pathFile = Path.Combine(Environment.CurrentDirectory, "Protocol.dat");
+            if (!File.Exists(pathFile)) return new List<Protocol>();
+
+            var file = File.ReadAllText(pathFile, Encoding.UTF8);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Protocol>>(file);
+        }
+
+        public static void SaveFile(List<Protocol> listProtocols)
+        {
+            var pathFile = Path.Combine(Environment.CurrentDirectory, "Protocol.dat");
+            File.WriteAllText(pathFile, Newtonsoft.Json.JsonConvert.SerializeObject(listProtocols.ToArray()));
+        }
+
+        private void btnViewProtocols_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in protocols)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            MessageBox.Show(sb.ToString(),"Protocolos");
+        }
+    }
+
+    public class Protocol
+    {
+        public string IdComapany { get; set; }
+        public string Date { get; set; }
+        public string Sequence { get; set; }
+
+        public override string ToString()
+        {
+            return IdComapany + Date + Sequence;
+        }
+    }
+}
